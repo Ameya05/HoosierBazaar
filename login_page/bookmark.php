aa8b6f3128c5e19d69125ce9135542558044fe72
rahuvela <rahul.vela@github.com>
2017-04-23 20:23:10 -0400

    php added for searching not including searching by name

--- login_page/bookmark.php
+++ login_page/bookmark.php
@@ -12,24 +12,13 @@ if ($conn->connect_error) {
 } 
 
 // sql to create table
-$sql = "CREATE TABLE product (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-name VARCHAR(100) NOT NULL,
-size VARCHAR(6) NOT NULL,
-price INT(6) NOT NULL,
-store_name VARCHAR(100) NOT NULL,
-is_discounted VARCHAR(10) DEFAULT 0,
-link VARCHAR(500),
-category VARCHAR(20),
-description VARCHAR(1000),
-brand VARCHAR(20),
-img_loc VARCHAR(1000),
-
-reg_date TIMESTAMP
+$sql = "CREATE TABLE Bookmarks (
+name VARCHAR(30) NOT NULL PRIMARY KEY,
+pid INT(30) NOT NULL
 )";
 
 if ($conn->query($sql) === TRUE) {
-    echo "Table Product created successfully";
+    echo "Table bookmark created successfully";
 } else {
     echo "Error creating table: " . $conn->error;
 }
---
+++ login_page/search.php
@@ -0,0 +1,75 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "test";
+$dbname = "myDB";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+#$name = 'jay';
+#$pwd = "password1";
+
+//$query = "SELECT * FROM Users WHERE name = '$name' and  pwd = '$pwd' ";
+
+
+#$size = 'S';
+#$brand = 'Columbia';
+#$category = 'Jackets';
+#$is_discounted = 'Yes';
+#$store_name = 'TIS Bookstore';
+
+$query = "SELECT * FROM product WHERE '1' = '1' ";
+
+if(isset($_GET["size"])){
+  $size = $_GET["size"];
+  $query = $query."and size = '$size' ";
+}
+if (isset($_GET["brand"])) {
+  # code...
+  $brand = $_GET["brand"];
+  $query = $query."and brand = '$brand' ";
+}
+if (isset($_GET["category"])) {
+  # code...
+  $category = $_GET["category"];
+  $query = $query."and category = '$category' ";
+}
+if (isset($_GET["is_discounted"])) {
+  # code...
+  $is_discounted = $_GET["is_discounted"];
+  $query = $query."and is_discounted = '$is_discounted' ";
+}
+if (isset($_GET["brand"])) {
+  # code...
+  $store_name = $_GET["store_name"];
+  $query = $query."and store_name = '$store_name' ";
+}
+
+
+
+
+//$query = $query. "name = '$name' and  pwd = '$pwd'";
+
+ if ($result=mysqli_query($conn,$query))
+  {
+   if(mysqli_num_rows($result) > 0)
+    {
+      echo "Exists";
+      #$row = $result->fetch_assoc();
+      #echo "<br> name: ". $row["name"]. " - Password: ". $row["pwd"]. " ";
+    }
+  else
+      echo "Doesn't exist";
+  }
+else
+    echo "Query Failed.";
+
+
+
+$conn->close();
+?>
